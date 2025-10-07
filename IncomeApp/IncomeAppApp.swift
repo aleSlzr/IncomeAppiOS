f32f0dca8867594909be859122e7c3147340cb6f
Alejandro Salazar <salazar.cruz.alejandro@gmail.com>
2025-10-07 14:04:35 -0600

    Initial Commit

---
+++ IncomeApp/IncomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeAppApp.swift
+//  IncomeApp
+//
+//  Created by Alejandro Salazar on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
